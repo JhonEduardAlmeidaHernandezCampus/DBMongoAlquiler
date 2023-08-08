999c3b591b18b01f0e3f4dc1bf9455825214c74a
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-07 20:53:10 -0500

    feat: :sparkles: Cliente GET POST
    Se agrega el get y post de cliente
    
    by: Jhon Hernandez

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ import express from 'express'
 import storageSucursal from './routes/sucursal.js';
 import storageSucursalAutomovil from './routes/sucursal_automovil.js';
 import storageReserva from './routes/reserva.js';
+import storageCliente from './routes/cliente.js';
 
 dotenv.config()
 let app = express();
 app.use(express.json());
 
-app.use("/sucursal", storageSucursal)
-app.use("/sucursal_automovil", storageSucursalAutomovil)
-app.use("/reserva", storageReserva)
+app.use("/sucursal", storageSucursal);
+app.use("/sucursal_automovil", storageSucursalAutomovil);
+app.use("/reserva", storageReserva);
+app.use("/cliente", storageCliente);
 
 let config = JSON.parse(process.env.Server)
 app.listen(config, () => console.log(`http://${config.hostname}:${config.port}`))
---
+++ routes/cliente.js
@@ -0,0 +1,63 @@
+import { Router } from 'express'
+import { con } from '../db/connect.js'
+import { configGET } from '../middleware/limit.js'
+
+let storageCliente = Router()
+let db = await con()
+
+storageCliente.get("/", configGET(), async(req, res) => {
+    try {
+        let tabla = db.collection("cliente");
+        let data = await tabla.find().toArray();
+        res.send(data)
+
+    } catch (error) {
+        res.status(402).send("Error al mostrar los datos de los clientes")
+    }
+})
+
+storageCliente.post("/", async(req, res) => {
+    try {
+        let tabla = db.collection("cliente")
+        await tabla.insertOne(req.body);
+        console.log(req.rateLimit);
+        res.send("Cliente creado con exito")
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Error al crear el registro del cliente, datos incorrectos")
+    }
+})
+
+// storageCliente.put("/:id", async(req, res) => {
+//     try {
+//         let { id } = req.params;
+//         let collection = db.collection("cliente")
+//         let res = await collection.bulkWrite([
+//             {
+//                 updateOne: {
+//                     filter: { _ID_Sucursal: Object.values(id) },
+//                     update: { $set: req.body }
+//                 }
+//             }
+//         ])
+//         res.send("Registro actualizado con exito")
+
+//     } catch (error) {
+//         res.status(400).send("Error al actualizar el registro")
+//     }
+// })
+
+// storageCliente.delete("/:id", async(req, res) => {
+//     try {
+//         let { id } = req.params;
+//         let collection = db.collection("cliente")
+//         let res = await collection.deleteOne({_ID_Sucursal: id})
+//         res.send("Registro eliminado con exito")
+
+//     } catch (error) {
+//         res.status(400).send("Error al eliminar el registro")
+//     }
+// })
+
+export default storageCliente;
