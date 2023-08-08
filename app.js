b4f2bbe13197a814e3a2693d7d7be5fa36a6b3cf
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-07 20:49:23 -0500

    feat: :sparkles: Sucursal Automovil GET POST
    Se agrega el get y post de sucursal automovil
    
    by: Jhon Hernandez

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import storageSucursal from './routes/sucursal.js';
+import storageSucursalAutomovil from './routes/sucursal_automovil.js';
 
 dotenv.config()
 let app = express();
 app.use(express.json());
 
 app.use("/sucursal", storageSucursal)
+app.use("/sucursal_automovil", storageSucursalAutomovil)
 
 let config = JSON.parse(process.env.Server)
 app.listen(config, () => console.log(`http://${config.hostname}:${config.port}`))
---
+++ routes/sucursal_automovil.js
@@ -0,0 +1,62 @@
+import { Router } from 'express'
+import { con } from '../db/connect.js'
+import { configGET } from '../middleware/limit.js'
+
+let storageSucursalAutomovil = Router()
+let db = await con()
+
+storageSucursalAutomovil.get("/", configGET(), async(req, res) => {
+    try {
+        let tabla = db.collection("sucursal_automovil");
+        let data = await tabla.find().toArray();
+        res.send(data)
+
+    } catch (error) {
+        res.status(402).send("Error al mostrar los datos de las sucursales de los automoviles")
+    }
+})
+
+storageSucursalAutomovil.post("/", configGET(), async(req, res) => {
+    try {
+        let tabla = db.collection("sucursal_automovil")
+        await tabla.insertOne(req.body);
+        console.log(req.rateLimit);
+        res.send("Sucursal de automovil creada con exito")
+
+    } catch (error) {
+        res.status(400).send("Error al crear el registro de la sucursal del automovil, datos incorrectos")
+    }
+})
+
+// storageSucursalAutomovil.put("/:id", async(req, res) => {
+//     try {
+//         let { id } = req.params;
+//         let collection = db.collection("sucursal_automovil")
+//         let res = await collection.bulkWrite([
+//             {
+//                 updateOne: {
+//                     filter: { _ID_Sucursal: Object.values(id) },
+//                     update: { $set: req.body }
+//                 }
+//             }
+//         ])
+//         res.send("Registro actualizado con exito")
+
+//     } catch (error) {
+//         res.status(400).send("Error al actualizar el registro")
+//     }
+// })
+
+// storageSucursalAutomovil.put("/:id", async(req, res) => {
+//     try {
+//         let { id } = req.params;
+//         let collection = db.collection("sucursal_automovil")
+//         let res = await collection.deleteOne({_ID_Sucursal: id})
+//         res.send("Registro eliminado con exito")
+
+//     } catch (error) {
+//         res.status(400).send("Error al eliminar el registro")
+//     }
+// })
+
+export default storageSucursalAutomovil;
