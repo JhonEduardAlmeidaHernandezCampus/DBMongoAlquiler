f330e8697b3137d2b29fd4f85d86472fe267f703
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-09 19:43:31 -0500

    feat: :sparkles: Connect
    Se cambia la conexion al servidor, ya que estaba presentando problemas de IP
    
    by: Jhon Hernandez

--- db/connect.js
+++ db/connect.js
@@ -5,7 +5,7 @@ dotenv.config("../");
 
 export async function con(){
     try {
-        const uri = `mongodb+srv://${process.env.User}:${process.env.Password}@cluster0.swlbwvz.mongodb.net/${process.env.Database}`
+        const uri = `mongodb+srv://${process.env.User}:${process.env.Password}@cluster0.rpldcx9.mongodb.net/${process.env.Database}`
         const option = {
             useNewUrlParser: true,
             useUnifiedTopology: true
