2aa2d0a2ca9a0f846159a9ac453c1ecffb49c4e9
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-13 20:29:42 -0500

    feat: :sparkles: App
    Actualizacion de todas las rutas
    
    by: Jhon Hernandez

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv'
 import express from 'express'
+
+import { generarToken, validarToken } from './tokens/tokens.js';
+
 import storageSucursal from './routes/sucursal.js';
 import storageAutomovil from './routes/automovil.js';
 import storageCliente from './routes/cliente.js';
@@ -10,30 +13,21 @@ import storageReserva from './routes/reserva.js';
 import storageRegistroDevolucion from './routes/registro_devolucion.js';
 import storageRegistroEntrega from './routes/registro_entrega.js';
 
-// Consultas
-import storageConsultaCliente from './routes/consultas/consultaCliente.js';
-import storageConsultaAutomovil from './routes/consultas/consultaAutomoviles.js';
-import storageConsultaAlquiler from './routes/consultas/consultaAlquiler.js';
-import storageConsultaEmpleado from './routes/consultas/consultaEmpleado.js';
-
 dotenv.config()
 let app = express();
 app.use(express.json());
 
-app.use("/sucursal", storageSucursal);
-app.use("/automovil", storageAutomovil);
-app.use("/cliente", storageCliente);
-app.use("/empleado", storageEmpleado);
-app.use("/alquiler", storageAlquiler);
-app.use("/sucursal_automovil", storageSucursalAutomovil);
-app.use("/reserva", storageReserva);
-app.use("/registro_devolucion", storageRegistroDevolucion);
-app.use("/registro_entrega", storageRegistroEntrega);
+app.use("/generarToken", generarToken)
 
-app.use("/consulta_cliente", storageConsultaCliente);
-app.use("/consulta_automovil", storageConsultaAutomovil);
-app.use("/consulta_alquiler", storageConsultaAlquiler);
-app.use("/consulta_empleado", storageConsultaEmpleado);
+app.use("/sucursal", validarToken, storageSucursal);
+app.use("/automovil", validarToken, storageAutomovil);
+app.use("/cliente", validarToken, storageCliente);
+app.use("/empleado", validarToken, storageEmpleado);
+app.use("/alquiler", validarToken, storageAlquiler);
+app.use("/sucursal_automovil", validarToken, storageSucursalAutomovil);
+app.use("/reserva", validarToken, storageReserva);
+app.use("/registro_devolucion", validarToken, storageRegistroDevolucion);
+app.use("/registro_entrega", validarToken, storageRegistroEntrega);
 
 let config = JSON.parse(process.env.Server)
 app.listen(config, () => console.log(`http://${config.hostname}:${config.port}`))
