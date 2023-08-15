450588de540e51f9d349e34b4ca87aef52b1c1b9
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-15 07:02:26 -0500

    feat: :bug: Connect.js
    Se crea una variable para parsear la conexion a la base de datos y se corrige
    
    by: Jhon Hernandez

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,11 @@ import dotenv from 'dotenv'
 import { MongoClient } from 'mongodb'
 
 dotenv.config("../");
+let config = JSON.parse(process.env.MY_CONNECT)
 
 export async function con(){
     try {
-        const uri = `mongodb+srv://${process.env.User}:${process.env.Password}@cluster0.rpldcx9.mongodb.net/${process.env.Database}`
+        const uri = `mongodb+srv://${config.user}:${config.password}@cluster0.rpldcx9.mongodb.net/${config.database}`
         const option = {
             useNewUrlParser: true,
             useUnifiedTopology: true
